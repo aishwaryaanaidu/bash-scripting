1df83d29f6962a5f926f1e362f078422ee3cec91
unknown <aishwarya13naidu@gmail.com>
2020-11-13 00:23:52 +0530

    Parsing the login API response and fetching the access token

--- cURL/login_and_server.sh
+++ cURL/login_and_server.sh
@@ -1,2 +1,8 @@
 response=$(curl -k --silent --header "Content-Type: application/json" --request POST --data '{"username":"admin","password":"Bet$0l@2020"}' https://127.0.0.1:8008/zmc_service/users/zmc/login/)
 access_token_object=`echo $response | grep -Po '"access":.*' | awk -F: '/"access":/ {print $2}'`
+# Remove suffix
+temp="${access_token_object%\}}"
+temp="${temp%\"}"
+# Remove prefix
+access_token="${temp#\"}"
+echo $access_token
