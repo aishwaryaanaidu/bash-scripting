dac2f5ae09c26d40882b6765a543fb8c9dc9f395
Aishwarya M S <38344621+aishwaryaanaidu@users.noreply.github.com>
2021-01-29 22:49:33 +0530

    Script to copy emails from Amazon SES

--- generic-scripts/amazon-ses.sh
+++ generic-scripts/amazon-ses.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+aws s3 cp --recursive s3://bucket-name/ /home/aishwarya.ms/s3-emails/temporary-emails/ --profile myaccount
 
+tmp_file_location=/home/aishwarya.ms/s3-emails/temporary-emails/*
+base_location=/home/aishwarya.ms/s3-emails/emails/
+
+today=$(date +"%m_%d_%Y")
+[[ -d ${base_location}/"$today" ]] || mkdir ${base_location}/"$today"
+
+for FILE in $tmp_file_location
+do
+   mv $FILE ${base_location}/${today}/email$(rand)
+done
