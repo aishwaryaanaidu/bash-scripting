9429fa69f3f7443d88e4a2561763905e2d9ab6dd
Aishwarya M S <38344621+aishwaryaanaidu@users.noreply.github.com>
2021-01-30 17:58:49 +0530

    Adding comments

--- generic-scripts/generate-ssh-keys.sh
+++ generic-scripts/generate-ssh-keys.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Taking the number of bits, type of key and filename from the user
 read -p "Enter the number of bits: "$'\n' bits
 read -p "Enter the type of key to create (dsa, ecdsa, ed25519 or rsa): "$'\n' type
 read -p "Enter the filename: "$'\n' filename
 
+# Using ssh-keygen and passing the values entered by the user to generate a public and private key pair
 ssh-keygen -b $bits -t $type -f $filename
